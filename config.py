45667e9ab5cb971be92c6359735ebf3aec4ca9e7
ɪꝛᴏ <im_maxim@inbox.ru>
2024-07-26 02:18:47 +0000

    Update config.py

--- config.py
+++ config.py
@@ -14,9 +14,9 @@ BOT_TOKEN = os.getenv("BOT_TOKEN", None)
 MONGO_URL = os.getenv("MONGO_URL", None)
 
 # Bot owner's Telegram user ID and username
-OWNER_ID = os.getenv("OWNER_ID",7178437397)
+OWNER_ID = os.getenv("OWNER_ID",6829300557)
 OWNER_USERNAME = "NoT_uR_SoHeL"
 
 # Support group and update channel names
-SUPPORT_GROUP = "Flames_xD"
+SUPPORT_GROUP = "SpIcYxChAtS"
 UPDATE_CHANNEL = "xD_Feelings"
